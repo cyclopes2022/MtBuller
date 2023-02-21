91e5ccbb2688ae5565374361c576e8e304c5c78c
cyclopes2022 <cyclopes2022@gmail.com>
2023-02-21 10:24:41 +0800

    add open-dataset: Structured Dataset

--- src/pages/data_analysis.py
+++ src/pages/data_analysis.py
@@ -4,6 +4,8 @@
 :LastEditTime: 2023-02-19 14:01:40
 :Description: 
 """
+# import fiftyone as fo
+# import fiftyone.zoo as foz
 import numpy as np
 import pandas as pd
 from pandas_profiling import ProfileReport
@@ -20,6 +22,18 @@ def pandas_profiling_report(df):
     df_report = ProfileReport(df, explorative=True)
     return df_report
 
+data_download_url = {
+    "Iris Dataset": "https://raw.githubusercontent.com/mwaskom/seaborn-data/master/iris.csv",
+    "Titanic Dataset": "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv",
+    "Wine Dataset": "https://raw.githubusercontent.com/GyJonatan/WineCSV/main/winequality-red.csv",
+}
+
+data_map = {
+            "Structured Dataset": ('Iris Dataset', 'Titanic Dataset', 'Wine Dataset'),
+            "Image Classification": ('CIFAR10 Dataset', 'CIFAR100 Dataset', 'Fashion MNIST Dataset'),
+            "Image Detection": ('COCO-2017 Dataset', 'VOC2007 Dataset', 'VOC2012 Dataset'),
+            "Image Segmentation": ('Cityscapes Dataset', 'CamVid Dataset', 'Mapillary Dataset'),
+}
 
 def write():
     # Global Variables
@@ -52,9 +66,51 @@ def write():
         st.subheader('Overview') 
 
     with tab_opendata:
-        df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv')
-        df_report = pandas_profiling_report(df)
-        st_profile_report(df_report)
+        # if "visibility" not in st.session_state:
+        #     st.session_state.visibility = "visible"
+        #     st.session_state.disabled = False
+
+        col1, col2 = st.columns(2)
+        
+        with col1:
+            st.checkbox("Disable selectbox widget", key="disabled")
+            st.radio(
+                "Please select a dataset type:",
+                key="data_type",
+                options=["Structured Dataset", "Image Classification", "Image Detection", "Image Segmentation"],
+            )
+
+        with col2:
+            option = st.selectbox(
+                "Please select the dataset to analyze:",
+                data_map[st.session_state.data_type],
+                label_visibility=st.session_state.visibility,
+                disabled=st.session_state.disabled,
+            )
+        if st.session_state.data_type == "Structured Dataset":
+            st.write("You selected:", st.session_state.data_type)
+            st.write("You selected:", option)
+            # read tatnic dataset through pandas
+
+
+            df = pd.read_csv(data_download_url[option])
+            # create figure
+            df_report = pandas_profiling_report(df)
+            st_profile_report(df_report)
+
+
+        if st.session_state.data_type == "Image Classification":
+            st.write("You selected:", st.session_state.data_type)
+            st.write("You selected:", option)
+        if st.session_state.data_type == "Image Detection":
+            st.write("You selected:", st.session_state.data_type)
+            st.write("You selected:", option)
+        if st.session_state.data_type == "Image Segmentation":
+            st.write("You selected:", st.session_state.data_type)
+            st.write("You selected:", option)
+        # df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_apple_stock.csv')
+        # df_report = pandas_profiling_report(df)
+        # st_profile_report(df_report)
         # train_df, test_df = train_test_split(df, train_size=0.8)
 
         # report = sweetviz.compare(source=train_df, compare=test_df, target_feat="Progression")
